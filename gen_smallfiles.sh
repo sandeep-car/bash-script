bf6d41375c50a9d67173607d32bcd6afbd84566b
Sandeep Cariapa <cariapa@gmail.com>
2019-07-26 12:55:45 -0700

    Create gen_smallfiles.sh

---
+++ gen_smallfiles.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+TARGDIR="/mnt/fs/smallfiles"
+# All file sizes are in bytes.
+MINFILESIZE=4096
+MAXFILESIZE=131072
+RANGE=$(($MAXFILESIZE-$MINFILESIZE+1));
+
+if [ $# -ne 1 ]
+then
+  echo "<Usage>: <$0> <Number of files to generate in $TARGDIR. File sizes between $MINFILESIZE and $MAXFILESIZE.>"
+  exit
+fi
+
+numfiles=$1
+
+i=0
+while [ $i -lt $numfiles ]
+do
+  filesize=$RANDOM
+  let "filesize %= $RANGE"
+  let filesize=filesize+$MINFILESIZE
+  if [ $(($i % 1000)) -eq 0 ]
+  then
+    echo "$i: $filesize"
+    date
+  fi
+  head -c $filesize /dev/urandom > $TARGDIR/$i
+  let i=i+1
+done
