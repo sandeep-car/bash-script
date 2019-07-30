1d93796cc7c04fb748cf2640277106dc963dfbd1
Sandeep Cariapa <cariapa@gmail.com>
2019-07-30 14:12:01 -0700

    Update gen_smallfiles.sh

--- gen_smallfiles.sh
+++ gen_smallfiles.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Mount point of NFS export.
 TARGDIR="/mnt/fs/smallfiles"
 # All file sizes are in bytes.
 MINFILESIZE=4096
 MAXFILESIZE=131072
+
+# ========== DO NOT CHANGE ANYTHING UNDER THIS LINE ========
 RANGE=$(($MAXFILESIZE-$MINFILESIZE+1));
 
 if [ $# -ne 1 ]
